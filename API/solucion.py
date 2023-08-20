759b137de665b8c7f9674870d2160a5abc1893b5
Lucas Morató Nieto <99823243+lucasmoratonieto@users.noreply.github.com>
2023-08-20 20:05:11 +0200

    Update solucion.py

--- API/solucion.py
+++ API/solucion.py
@@ -1,1 +1,11 @@
+import requests
 
+url = "https://pokeapi.co/api/v2/pokemon?limit=151"
+
+response = requests.get(url)
+
+for pokemon in response.json()["results"]:
+    print(pokemon["name"])
+
+
+#just test
