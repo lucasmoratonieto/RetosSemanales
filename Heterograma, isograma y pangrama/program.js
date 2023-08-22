605e55fae106b81bcb9d161240e88d55abf05f81
lukmorato@gmail.com <lukmorato@gmail.com>
2023-08-22 15:56:49 +0200

    Tab

---
+++ Heterograma, isograma y pangrama/program.js
@@ -0,0 +1,20 @@
+/*
+ * Crea 3 funciones, cada una encargada de detectar si una cadena de
+ * texto es un 
+    heterograma -> Un heterograma es una palabra o frase que no contiene ninguna letra repetida
+    isograma ->  palabra o frase en la que cada letra aparece el mismo número de veces
+    pangrama -> Un pangrama o frase holoalfabética es un texto que usa todas las letras posibles del alfabeto de un idioma.
+ * - Debes buscar la definición de cada uno de estos términos.
+ */
+ 
+ 
+ let phrase = "lucas"
+ function heterograma(){
+    phrase = phrase.toLowerCase();
+    const phraseArr = phrase.split("");
+    //Check point, it split the phrase in a arr
+    //console.log(phraseArr);
+    
+ }
+ 
+ heterograma();
